3da0c4983231e337e762c79794d72ebae5aff683
Enes Özdogan <enes.oezdogan2@gmail.com>
2019-11-17 19:09:40 +0100

    detail Ansicht hinzugefügt

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Cow } from '@shared'
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CowService, CartItemService, Cow } from '@shared';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-detail',
@@ -9,21 +9,35 @@ import { Cow } from '@shared'
     styleUrls: ['./detail.component.scss']
 })
 
-export class DetailComponent implements OnInit{
+export class DetailComponent implements OnInit {
     activatedRoute: ActivatedRoute;
     cow: Cow;
     id: string;
     serviceSub: any;
 
-    constructor(private route: ActivatedRoute) { 
-        
+    constructor(private route: ActivatedRoute,
+         private readonly cowservice: CowService,
+          private readonly cartItemService: CartItemService){
+
     }
 
     ngOnInit(): void{
+        this.id = this.route.snapshot.paramMap.get('id');
+            
+        this.serviceSub = this.cowservice.getCow(this.id).subscribe((cow) =>{
+            this.cow = cow;
+        });
+        console.log(this.serviceSub);
 
     }
+    public onSubmit(form: NgForm): void {
+        const gewicht = form.value.gewicht;
+        const amount = form.value.amount;
+        const fleshPiece = form.value.fleshPiece;
+        this.cartItemService.addToCart(this.cow,gewicht,fleshPiece,250,amount);
+       }
 
-    ngOnDestroy(): void{
-
-    }
+    ngOnDestroy(){
+        this.serviceSub.unsubscribe();
+      }
 }

f8a7cb22534ada33ede8926d3d128b0b4d1a6f93
theothies <theodor.t1999@gmail.com>
2019-11-17 13:07:55 +0100

    Erstellung Modell Carousel

---
+++ src/app/shared/models/carousel.model.ts
@@ -0,0 +1,12 @@
+export class Carousel{
+    constructor(
+        public imageUrl: string,
+        public text: string,
+        public age: number,
+        public race: string,
+        public hof: string,
+        public alt: string,
+        public link: string,
+        public isLinkExternal: boolean
+    ){}
+}

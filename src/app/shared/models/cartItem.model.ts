bc099834e2a514cc1ad91d0be1a64ec8ae71a057
Robin Böhringer <robin.boehringer@t-online.de>
2019-11-17 12:49:06 +0100

    CartItem Model hinzugefügt

---
+++ src/app/shared/models/cartItem.model.ts
@@ -0,0 +1,12 @@
+export class CartItem{
+    constructor(
+        public id: number,
+        public name: string,
+        public origin: string,
+        public weight: number,
+        public sex: string,
+        public price: number,
+        public amount: number,
+        public fleshPiece: string,
+        ){}
+}
